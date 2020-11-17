61fd6026951e4d0b88019d7bc2feac145c634e06
jpedroc <jpedrocalixto@outlook.com>
2020-11-17 17:44:29 -0300

    mudando classes

--- src/prova1/Prontuario.java
+++ src/prova1/Prontuario.java
@@ -3,22 +3,29 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package prova1;
+package clientmedudp;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 
 /**
  *
  * @author luizn
  */
-public class ModelMensagem {
+public class Prontuario extends Object implements Serializable{
     private String doenca;
     private ArrayList<String> sintomas; 
 
-    public ModelMensagem(ArrayList sintomas, String deoenco) {
+    public Prontuario(ArrayList sintomas, String deoenco) {
         this.sintomas = sintomas;
         this.doenca = deoenco;
     }    
+
+    public Prontuario(ArrayList<String> sintomas) {
+        this.sintomas = sintomas;
+        this.doenca = "";
+    }
+    
 
     public ArrayList<String> getSintomas() {
         return sintomas;
@@ -36,5 +43,13 @@ public class ModelMensagem {
         this.doenca = doenca;
     }
 
+    @Override
+    public String toString() {
+        String message = doenca;
+        for (String sintoma: sintomas) {
+            message = message.concat(" - ").concat(sintoma);
+        }
+        return message;
+    }
     
 }
